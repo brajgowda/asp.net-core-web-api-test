06098004d57e2a7b763c33b5609680cc784054b5
brajgowda <44913194+brajgowda@users.noreply.github.com>
2020-04-20 20:45:58 -0400

    Create The CelestialObjectController Class
    completed 4th Module
    4. Create The CelestialObjectController Class

---
+++ pluralsight-projects-AspNetCore-StarChartAPI-29dc253/StarChart/Models/CelestialObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+        
+        // Satellite list
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
+        
+        // to calculate time
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
