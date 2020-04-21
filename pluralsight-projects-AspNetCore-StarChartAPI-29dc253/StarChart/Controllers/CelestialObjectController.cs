9f3bd8a1c87e0e3bd0b2e8ccb57767cb1c594c93
brajgowda <44913194+brajgowda@users.noreply.github.com>
2020-04-20 20:54:21 -0400

    added comments

--- pluralsight-projects-AspNetCore-StarChartAPI-29dc253/StarChart/Controllers/CelestialObjectController.cs
+++ pluralsight-projects-AspNetCore-StarChartAPI-29dc253/StarChart/Controllers/CelestialObjectController.cs
@@ -7,6 +7,11 @@ using StarChart.Data;
 
 namespace StarChart.Controllers
 {
+ // 4)   Create CelestialObjectController class
+ //Create a new class CelestialObjectController in the Controllers folder that inherits the ControllerBase class. If any actions are automatically generated they should be removed. (Note: you will need to add a using directive for Microsoft.AspNetCore.Mvc)
+ //Add a Route attribute with a value of "" and ApiController attribute to the CelestialObjectController.
+ //Create a new private readonly field of type ApplicationDbContext named _context. (Note: you will need to add a using directive for StarChart.Data)
+ //Create a constructor that accepts a parameter of type ApplicationDbContext and sets the _context field using the provided parameter.
 
     [Route("")]
     [ApiController]
--- pluralsight-projects-AspNetCore-StarChartAPI-29dc253/StarChart/Models/CelestialObject.cs
+++ pluralsight-projects-AspNetCore-StarChartAPI-29dc253/StarChart/Models/CelestialObject.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 
 namespace StarChart.Models
 {
+
+ //  3) Create CelestialObject Model task
+ //Create a new public class CelestialObject in the Models directory
+ //Create a new public property of type int named Id.
+ //Create a new public property of type string named Name.This property should have the Required attribute. (Note: you will need to add a using directive for Systems.ComponentModel.DataAnnotations)
+ //Create a new public property of type int? named OrbitedObjectId.
+ //Create a new public property of type List<CelestialObject> named Satellites. This property should have the NotMapped attribute. (Note: you will need to add using directives for System.Collections.Generic and System.ComponentModels.DataAnnotations.Schema)
+ //Create a new public property of type TimeSpan named OrbitalPeriod.
+ //In the ApplicationDbContext class, located in the Data folder, create a new public property of type DbSet<CelestialObject> named CelestialObjects. (Note: you will need to add a using directive for StarChart.Models)
     public class CelestialObject
     {
         public int Id { get; set; }
